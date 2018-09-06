cdd6c0a8f888ecadd1011774a88e78241504bad8
Fabian Wetzel <fabian.wetzel@vattenfall.de>
2018-09-06 11:59:12 +0000

    added working first version

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
 # ingester
+
 a script to ingest media files from a memory card in an opinionated projects structure
+
+## What can ingester do for you?
+
+When cutting videos, I have to copy video files from memory cards into the project structure several times.
+Ingester can help you do this by renaming video files, selecting (and creating) the correct subfolder
+and by opening the ingested media folder after execution.
+
+## What is opinionated in ingester?
+
+Ingester assumes some facts about your PC and your folder structure:
+
+- You use a Windows PC (ingester ist 90% cross-platform but there are probably some adaptations needed. Make a PR if you want!)
+- Your memory card always gets the same drive letter. Ingester uses a configured source folder.
+- All your relevant video projects have the same parent folder
+- Every video project has its own folder
+- Every ingest is done in a new subfolder within a project
+
+### Projects structure
+
+```text
+C:\video projects
+├───Project A
+│   ├───raw media 1
+│   ├───raw media 2
+│   └───raw media 3
+├───Project B
+│   ├───raw media 1
+│   ├───raw media 2
+│   └───raw media 3
+└───Project C
+    ├───raw media 1
+    ├───raw media 2
+    └───raw media 3
+```
+
+## Show me
+
+### install and first global run
+
+```powershell
+cd c:\scripts
+git clone https://github.com/fabsenet/ingester.git
+cd ingester
+npm install
+node index.js
+```
+
+> please edit the ingester.conf.json before doing any ingests!
+
+The explorer window opens with the ingester.conf.json file preselected. Edit this file your favorit text editor
+and provide your source directory and your projects directory:
+
+The following sample assumes your memory card always gets the drive letter J: and the projects directory is the
+same as above:
+
+```json
+{
+  "projectsDir": "C:\\video projects",
+  "sourceDir": "J:\\",
+  "sourceFilters": ["**/*.mp4"]
+}
+```
+
+### first ingest run for a project
+
+After inserting the memory card, run `node index.js`:
+
+```text
+? Select the project to ingest in » - Use arrow-keys. Return to submit.
+>  Project C
+   Project B
+   Project A
+```
+
+Ingester lists all Projects order by last modified date descending. This assumes you have some old projects but always
+working on the new ones. Selecting `Project C` yields:
+
+```text
+√ Select the project to ingest in » Project C
+the target is Project C
+the config for the project Project C must be created!
+? Enter the prefix the ingested files should get »
+```
+
+Ingester wants to know the prefix for renaming the files. I enter `PC_`:
+
+```text
+searching in J:\ for files to ingest.
+this is ingest 1 for this project.
+targetFolderPath is C:\video projects\Project C\raw 1\
+preparing to ingest 6 files with a size of 238 mb.
+[====================================] 100% 0.0s
+```
+
+It now has copied all media files from the memory card and opens an explorer window preselecting the new folder.
+`raw 1` in this case. You are now free to format the memory card and to add the media files into your video editor or whatever.
+
+### every next run for a project
+
+After the project is initialized, ingester will present the project selection as you may want to ingest for another
+project instead and simply do its job without further asking any other stuff. It will always open the folder after ingesting.
---
+++ index.js
@@ -0,0 +1,132 @@
+const chalk = require("chalk");
+const prompts = require("prompts");
+const gracefulFs = require("graceful-fs");
+const util = require("util");
+const path = require("path");
+const makeDir = require("make-dir");
+const log = console.log;
+const exec = require("child_process").exec;
+const cpy = require("cpy");
+const ProgressBar = require("progress");
+const globby = require("globby");
+
+const fsReadDir = util.promisify(gracefulFs.readdir);
+const fsExists = util.promisify(gracefulFs.exists);
+const fsStat = util.promisify(gracefulFs.stat);
+const fsRealpath = util.promisify(gracefulFs.realpath);
+const fsReadfile = util.promisify(gracefulFs.readFile);
+const fsWritefile = util.promisify(gracefulFs.writeFile);
+
+(async () => {
+  //TODO add global conf!
+  const globalConfigFilename = path.resolve(process.env.USERPROFILE, "ingester.conf.json");
+  let globalConfig;
+  if (await fsExists(globalConfigFilename)) {
+    globalConfig = JSON.parse(await fsReadfile(globalConfigFilename));
+  } else {
+    globalConfig = { projectsDir: "", sourceDir: "", sourceFilters: ["**/*.mp4"] };
+    await fsWritefile(globalConfigFilename, JSON.stringify(globalConfig, undefined, 1));
+  }
+  if (!globalConfig.projectsDir) {
+    exec(`explorer.exe /select,"${globalConfigFilename}"`);
+    log(chalk.red("please edit the ingester.conf.json before doing any ingests!"));
+    return;
+  }
+
+  let projectDirectories;
+  try {
+    projectDirectories = await fsReadDir(globalConfig.projectsDir);
+  } catch {
+    throw new Error(`failed to list projects in dir "${globalConfig.projectsDir}".`);
+  }
+
+  //remove with underscore
+  projectDirectories = projectDirectories.filter(f => !f.startsWith("_"));
+
+  //map to fullnames
+  projectDirectories = await Promise.all(
+    projectDirectories.map(async p => {
+      return { name: p, fullPath: await fsRealpath(globalConfig.projectsDir + "/" + p) };
+    })
+  );
+
+  //get stats
+  projectDirectories = await Promise.all(
+    projectDirectories.map(async p => {
+      return { ...p, stat: await fsStat(p.fullPath) };
+    })
+  );
+
+  //remove files
+  projectDirectories = projectDirectories.filter(f => f.stat.isDirectory());
+
+  //sort by modification time desc
+  projectDirectories = projectDirectories.sort((f1, f2) => (f1.stat.mtime > f2.stat.mtime ? -1 : 1));
+
+  const projectDir = (await prompts({
+    type: "select",
+    name: "value",
+    message: "Select the project to ingest in",
+    initial: 0,
+    choices: projectDirectories.map(p => ({ title: p.name, value: p }))
+  })).value;
+
+  log("the target is " + chalk.green(projectDir.name));
+  //load ingest.conf.json from project folder
+
+  let config;
+
+  const configFile = projectDir.fullPath + "/ingest.conf.json";
+  if (await fsExists(configFile)) {
+    //load existing config
+    const configContent = await fsReadfile(configFile);
+    config = JSON.parse(configContent);
+  } else {
+    //query user for new config
+    log("the config for the project " + chalk.green(projectDir.name) + " must be created!");
+
+    config = await prompts({
+      type: "text",
+      name: "prefix",
+      message: "Enter the " + chalk.yellow("prefix") + " the ingested files should get"
+    });
+    if (!config.prefix) {
+      log(chalk.red("project configuration initialisation cancelled!"));
+      return;
+    }
+    config.ingestCount = 0;
+  }
+  log(`searching in ${chalk.green(globalConfig.sourceDir)} for files to ingest.`);
+  var absoluteFiles = await globby(globalConfig.sourceFilters, { cwd: globalConfig.sourceDir, absolute: true, stats: true });
+  if (absoluteFiles.length === 0) {
+    log(chalk.red("Could not find any files to ingest."));
+    return;
+  }
+
+  config.ingestCount++;
+  log(`this is ingest ${chalk.green(config.ingestCount)} for this project.`);
+  const targetFolderPath = `${projectDir.fullPath}/raw ${config.ingestCount}/`.replace(/\//g, "\\");
+  log(`targetFolderPath is ${chalk.green(targetFolderPath)}`);
+
+  await makeDir(targetFolderPath);
+
+  var totalSize = absoluteFiles.reduce((prev, cur) => prev + cur.size, 0);
+
+  log(`preparing to ingest ${chalk.green(absoluteFiles.length)} files with a size of ${chalk.green((totalSize / 1024.0 / 1024.0).toPrecision(3))} mb.`);
+
+  var copyProgress = new ProgressBar("[:bar] :percent :etas", { complete: chalk.green("="), total: totalSize, width: 35, incomplete: " " });
+
+  var lastCompletedSize = 0;
+  await cpy(globalConfig.sourceFilters, targetFolderPath, {
+    cwd: globalConfig.sourceDir,
+    overwrite: false,
+    rename: p => `${config.prefix}${p}`
+  }).on("progress", progress => {
+    copyProgress.tick(progress.completedSize - lastCompletedSize);
+    lastCompletedSize = progress.completedSize;
+  });
+
+  await fsWritefile(configFile, JSON.stringify(config));
+
+  exec(`explorer.exe /select,"${targetFolderPath}"`);
+})();
